a39050a26d860bcf410fc6e5b6f1804bc6215d6e
Vincent Herrin <vherrin@us.ibm.com>
2016-05-11 08:54:46 -0500

    update to 5.03.2016

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "WatsonDeveloperCloud",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 11),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 6, minor: 1)
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 7, minor: 0)
     ],
     targets: [
         Target(name: "RestKit"),
