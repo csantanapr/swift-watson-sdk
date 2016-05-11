9aea53e14c3508880f6d5ffa25655918b6fdd862
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-05-11 12:19:45 -0500

    Update Kitura to 0.13

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "WatsonDeveloperCloud",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 13),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 7, minor: 0)
     ],
     targets: [
