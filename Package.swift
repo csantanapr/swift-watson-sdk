abe7f42f987be930175fdcb32451768a3fe54a8f
Vincent Herrin <vherrin@us.ibm.com>
2016-09-24 15:27:36 -0500

    update Kitura-net to version 1.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         Target(name: "AlchemyVision", dependencies: [.Target(name: "RestKit")])
     ],
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 32),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 14)
     ]
 )
